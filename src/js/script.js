876f38ce6dc996131e5da4934827166253b72667
devalade <aladecharaf23@gmail.com>
2022-05-02 03:12:45 +0000

    Deploy https://github.com/devalade/Furniture-Shop-website/commit/71f717598739440bf0bae520b97044c87ee533fb

--- src/js/script.js
+++
@@ -1,6 +0,0 @@
-import Alpine from 'alpinejs';
-
-window.addEventListener('loadeddata', () => {
-  const allImages = document.querySelectorAll('[data-preview-image]');
-  console.log(allImages);
-});
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.astro"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
